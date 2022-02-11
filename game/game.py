519b52a6073099a128ca0a7dd1c9bdddd90df072
rodrigoloyo <rmloyo@byui.edu>
2022-02-10 19:23:18 -0600

    Final game

--- game/game.py
+++ game/game.py
@@ -7,8 +7,9 @@ class Game(object):
 
     
     def __init__(self):
-        filename = "game\word_list.txt"
-        self.secret_word = RandomWord(filename)
+        
+        self.random = RandomWord().getter()
+        self.secret_word = RandomWord()
         self.jumperman = Jumperman()
         self.num_incorrect_guesses_made = 0
         self.guessed_letters = [ ]
@@ -24,7 +25,7 @@ class Game(object):
         while self._is_playing:
             self._get_inputs()
             self._do_updates()
-            self._do_outputs()
+             
     
 
 
@@ -36,14 +37,13 @@ class Game(object):
         """
 
         print()
-
-
+ 
         self.letter_guess = input("Guess a letter: ")
 
         self.guessed_letters.append(self.letter_guess)
         print(self.guessed_letters)
 
-
+         
        
         
     def _do_updates(self):
@@ -52,30 +52,8 @@ class Game(object):
             Args:
                 self (Director): An instance of Director.
             """
-            #check in ramdom words or new class to do the calculation
-
-            #if letter is in the ramdom word continue otherwise delete it and delete one life from the Jumper pics
-
-        
-     
-
-
-
-            # if len(self.guessed_letters) == 0:               
-
-            #     self.secret_word.display(self.guessed_letters)
-            # letter_guess = input("Please guess a letter: ").lower()
-            
-
-
-            # if letter_guess not in self.guessed_letters:
-            #     self.guessed_letters.append(letter_guess)
-
-            
          
-             
-
-            if self.letter_guess not in self.secret_word.word:
+            if self.letter_guess not in self.random:
                    
                     self.num_incorrect_guesses_made += 1
                     self.jumperman.draw(self.num_incorrect_guesses_made)
@@ -86,25 +64,26 @@ class Game(object):
                     print ("You chose wisely!\n")
                     self.jumperman.draw(self.num_incorrect_guesses_made)
          
-            print ("You've already guessed the letter '%s', please choose a new letter." % self.guessed_letters)
+            # print ("You've already guessed the letter '%s', please choose a new letter." % self.guessed_letters)
 
             current_word = self.secret_word.display(self.guessed_letters)
             print ("These are the letters you've already choosen so far: ", self.guessed_letters)
             if "_" not in current_word:
                 print ("Congratulations! You've won!")
+                self._is_playing = False
        
    
         
-    def _do_outputs(self):
-        """Provides a hint for the seeker to use.
+    # def _do_outputs(self):
+    #     """Provides a hint for the seeker to use.
 
-        Args:
-            self (Director): An instance of Director.
+    #     Args:
+    #         self (Director): An instance of Director.
 
 
-        """
-        if not self._is_playing:
-            return
+    #     """
+    #     if not self._is_playing:
+    #         return
          
         
       
--- game/ramdomword.py
+++ game/ramdomword.py
@@ -1,32 +1,35 @@
 import random
+import re
 
 class RandomWord(object):
     """
     read the words from an outside file and return one of those words, randomly chosen
     """
-    def __init__(self, filename):
-        with open(filename) as f:
-            words_list = []
-            for line in f:
-                words_list.append(line.rstrip('\r\n'))
-            self.word = random.choice(words_list)
-            print(words_list)
-            print(self.word)
+    def __init__(self):
+        
+            word_list = ["anarchism", "capitalism", "communism", "conservatism"]
+            self._word = random.choice(word_list)
+            
+            
+            
 
     def display(self, guessed_letters_list):
         """
         Function to display the secret word with the letters that have been correctly guessed.
         """
         word_to_display = ""
-        for letter in self.word:
+        for letter in self._word:
             if letter in guessed_letters_list:
                 word_to_display += " " + letter + " "
             else:
                 word_to_display += " _ "
         print ("This is your word so far:", word_to_display, "\n")
         return word_to_display
+    def getter(self):
 
+        
 
+       return self._word 
 
 
         
